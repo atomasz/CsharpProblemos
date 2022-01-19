f0fc03e0ce71f89545058b3e473bc929303878fc
atomasz <aaron.tomasz@gmail.com>
2022-01-19 23:26:45 +0000

    problemo one

---
+++ Problemos/Problemos/MergeLinkedList.cs
@@ -0,0 +1,48 @@
+﻿namespace Problemos;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+public class MergeLinkedList
+{
+    public ListNode MergeKLists(ListNode[] lists)
+    {
+        var numberOfLists = lists.Length;
+
+        if (numberOfLists == 0)
+            return null;
+
+        if (numberOfLists == 1)
+            return lists[0];
+
+        for (var i = 0; i < numberOfLists - 1; i++)
+        {
+            lists[0] = SortTwoLists(lists[0], lists[i + 1]);
+        }
+
+        return lists[0];
+    }
+
+    private ListNode SortTwoLists(ListNode listOne, ListNode listTwo)
+    {
+        if (listOne == null)
+            return listTwo;
+
+        if (listTwo == null)
+            return listOne;
+
+        var leftIsLower = listOne.val < listTwo.val;
+        var head = leftIsLower ? listOne : listTwo;
+        head.next = leftIsLower ? SortTwoLists(listOne.next, listTwo) : SortTwoLists(listOne, listTwo.next);
+        return head;
+    }
+}
