cd536f84942024352fcbea1536b2f9380d3102e4
atomasz <aaron.tomasz@gmail.com>
2022-01-23 18:00:27 +0000

    median scrap book

---
+++ Problemos/Problemos/Median.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problemos
+{
+    public static class Median
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var combinedLength = nums1.Length + nums2.Length;
+
+            var needAv = combinedLength % 2 == 0;
+            var nums1Index = 0;
+            var nums2Index = 0;
+            var lengthToGo = (combinedLength / 2);
+
+            if (nums1.Length == 0)
+            {
+                if (!needAv)
+                {
+                    return nums2[lengthToGo];
+                }
+                else
+                {
+                    return (double)(nums2[lengthToGo] + nums2[lengthToGo - 1]) / 2;
+                }
+            }
+
+            if (nums2.Length == 0)
+            {
+                if (!needAv)
+                {
+                    return nums1[lengthToGo];
+                }
+                else
+                {
+                    return (double)(nums1[lengthToGo] + nums1[lengthToGo - 1]) / 2;
+                }
+            }
+
+            var firstIndexValue = 0;
+            var secIndexValue = 0;
+            while ((nums1Index + nums2Index) <= (lengthToGo))
+            {
+                if (nums1Index == nums1.Length)
+                {
+                    secIndexValue = firstIndexValue;
+                    firstIndexValue = nums2[nums2Index];
+                    nums2Index++;
+                    continue;
+                }
+                else if (nums2Index == nums2.Length)
+                {
+                    secIndexValue = firstIndexValue;
+                    firstIndexValue = nums1[nums1Index];
+                    nums1Index++;
+                    continue;
+                }
+
+
+                var nums1Value = nums1[nums1Index];
+                var nums2Value = nums2[nums2Index];
+                secIndexValue = firstIndexValue;
+                if (nums1Value < nums2Value)
+                {
+                    firstIndexValue = nums1Value;
+                    nums1Index++;
+                }
+                else
+                {
+                    firstIndexValue = nums2Value;
+                    nums2Index++;
+                }
+            }
+
+            if (!needAv)
+                return firstIndexValue;
+
+            return (double)(firstIndexValue + secIndexValue) / 2;
+        }
+    }
+}
--- Problemos/Problemos/Program.cs
+++ Problemos/Problemos/Program.cs
@@ -1,26 +1,38 @@
 ﻿using Problemos;
 
-var random = new Random();
-var randomLength = random.Next((int)(5 * Math.Pow(10, 1)));
-var chars = "qwertyuioplkjhgfdsazxcvbnm";
-var stringChars = new char[randomLength];
+//var random = new Random();
+//var randomLength = random.Next((int)(5 * Math.Pow(10, 1)));
+//var chars = "qwertyuioplkjhgfdsazxcvbnm";
+//var stringChars = new char[randomLength];
 
-for (int i = 0; i < stringChars.Length; i++)
-{
-    stringChars[i] = chars[random.Next(chars.Length)];
-}
+//for (int i = 0; i < stringChars.Length; i++)
+//{
+//    stringChars[i] = chars[random.Next(chars.Length)];
+//}
 
-var finalString = new String(stringChars);
+//var finalString = new String(stringChars);
 
-var test = ShortestPalindromeResult.RandomMusings("aba");
-Console.WriteLine(test);
-
-
-
-Console.WriteLine($"Random string with length: {randomLength}, value: {finalString}");
+//var test = ShortestPalindromeResult.RandomMusings("aba");
+//Console.WriteLine(test);
 
 
-var calcResult = Calculator.Calculate("- (3 + (4 + 5))");
-var calcResult2 = Calculator.Calculate("(1+(4+5+2)-3)+(6+8)");
 
-Console.WriteLine($"Calc result = {calcResult}, {calcResult2}");
+//Console.WriteLine($"Random string with length: {randomLength}, value: {finalString}");
+
+
+//var calcResult = Calculator.Calculate("- (3 + (4 + 5))");
+//var calcResult2 = Calculator.Calculate("(1+(4+5+2)-3)+(6+8)");
+
+//Console.WriteLine($"Calc result = {calcResult}, {calcResult2}");
+
+
+
+
+var median = Median.FindMedianSortedArrays(new int[] { }, new int[] { 2 });
+var median2 = Median.FindMedianSortedArrays(new int[] { 2}, new int[] {  });
+var median3 = Median.FindMedianSortedArrays(new int[] { 1 }, new int[] { 2 });
+var median4 = Median.FindMedianSortedArrays(new int[] { 1}, new int[] { 2,3});
+var median5 = Median.FindMedianSortedArrays(new int[] { 1,2,3}, new int[] { 4,5,6});
+var median6 = Median.FindMedianSortedArrays(new int[] { 1, 2, 4 }, new int[] { 3, 5, 6, 7 });
+
+Console.WriteLine($"Med result = {median}, {median2}, {median3}, {median4}, {median5}, {median6}");
